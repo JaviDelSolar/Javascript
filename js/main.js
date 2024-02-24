0428c20fdb3271cf03fcf18a25f027efa05f8bda
Javiera Del Solar <javii.dsr@gmail.com>
2024-02-23 23:28:28 -0300

    Me gusta

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Esta constante se refiere a todos los elementos del botón "Me gusta" en el documento
+//'document.querySelectorAll('.like-btn') para seleccionar todos los elementos que tienen la clase like-btn 
+//(todos los botones "Me gusta").
+const likeBtns = document.querySelectorAll('.like-btn');
+
+//El bucle forEach recorre todos los botones "Me gusta" y ejecuta la función proporcionada para cada uno de ellos
+likeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const parentDiv = btn.parentElement;
+        const likeCount = parentDiv.querySelector('.like-count');
+        let likes = parseInt(likeCount.textContent);
+        likes++;
+        likeCount.textContent = likes + ' Me gusta';
+    });
+});
+
