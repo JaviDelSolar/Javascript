5bf18c452e30acc488224323cfbf66f4ea9129e0
Javiera Del Solar <javii.dsr@gmail.com>
2024-03-04 20:03:57 -0300

    Se agrega al html div id saludo para formulario de ingresa tu nombre

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-//Esta constante se refiere a todos los elementos del botón "Me gusta" en el documento
-//'document.querySelectorAll('.like-btn') para seleccionar todos los elementos que tienen la clase like-btn 
-//(todos los botones "Me gusta").
+// Array para almacenar la cantidad de "Me gusta" de cada imagen
+let likesArray = [];
+
 const likeBtns = document.querySelectorAll('.like-btn');
 
-//El bucle forEach recorre todos los botones "Me gusta" y ejecuta la función proporcionada para cada uno de ellos
-likeBtns.forEach(btn => {
+// El bucle forEach recorre todos los botones "Me gusta" y ejecuta la función proporcionada para cada uno de ellos
+likeBtns.forEach((btn, index) => {
     btn.addEventListener('click', () => {
         const parentDiv = btn.parentElement;
         const likeCount = parentDiv.querySelector('.like-count');
@@ -12,8 +12,31 @@ likeBtns.forEach(btn => {
         likes++;
         likeCount.textContent = likes + ' Me gusta';
 
-        alert('¡Gracias por tu Me gusta!');
+        // Almacenar la cantidad de "Me gusta" en el array
+        likesArray[index] = likes;
+
+        // Verificar la cantidad de "Me gusta" para la imagen actual
+        const totalLikes = likesArray[index];
+
+        if (totalLikes === 1) {
+            alert('¡Gracias por tu primer Me gusta!');
+        } else if (totalLikes === 3) {
+            alert('¡Muchas gracias! Ya me has dado 3 Me gusta.');
+        } else {
+            alert('¡Gracias por tu Me gusta!');
+        }
     });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const name = prompt('Por favor, ingresa tu nombre:');
+    const saludoDiv = document.getElementById('saludo');
+
+    if (name) {
+        saludoDiv.textContent = '¡Hola ' + name + ', soy Rami!';
+    } else {
+        saludoDiv.textContent = 'Hola, soy Rami.';
+    }
 });
 
 
