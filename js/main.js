6e9a026552e1cfc5b1a78b2d450dfe5c780899ce
Javiera Del Solar <javii.dsr@gmail.com>
2024-03-18 18:06:29 -0300

    Se reemplaza textContent po innerText

--- js/main.js
+++ js/main.js
@@ -13,17 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Mostrar los "Me gusta" almacenados en localStorage en los contadores
     likeCounts.forEach((count, index) => {
-        count.textContent = likesArray[index] + ' Me gusta';
+        count.innerText = likesArray[index] + ' Me gusta';
     });
 
     // Agregar eventos de clic a los botones "Me gusta"
     likeBtns.forEach((btn, index) => {
         btn.addEventListener('click', () => {
             likesArray[index]++;
-            likeCounts[index].textContent = likesArray[index] + ' Me gusta';
+            likeCounts[index].innerText = likesArray[index] + ' Me gusta';
             guardarLikes();
         });
     });
-
 });
 
