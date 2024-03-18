ad743ab3cdbdad9b11009e71817fe2c8d30a0b62
Javiera Del Solar <javii.dsr@gmail.com>
2024-03-18 18:00:39 -0300

    Se modifica el codigo para mostrar los me gustas almacenados en localStorage en los contadores

--- js/main.js
+++ js/main.js
@@ -1,43 +1,29 @@
-// Array para almacenar la cantidad de "Me gusta" de cada imagen
-let likesArray = [];
+document.addEventListener('DOMContentLoaded', function () {
+    // Acá obtengo los botones "Me gusta" y los contadores
+    const likeBtns = document.querySelectorAll('.like-btn');
+    const likeCounts = document.querySelectorAll('.like-count');
 
-const likeBtns = document.querySelectorAll('.like-btn');
+    // Acá obtengo los "Me gusta" almacenados en localStorage o inicializarlos a cero
+    let likesArray = JSON.parse(localStorage.getItem('likes')) || Array.from({ length: likeBtns.length }, () => 0);
 
-// El bucle forEach recorre todos los botones "Me gusta" y ejecuta la función proporcionada para cada uno de ellos
-likeBtns.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        const parentDiv = btn.parentElement;
-        const likeCount = parentDiv.querySelector('.like-count');
-        let likes = parseInt(likeCount.textContent);
-        likes++;
-        likeCount.textContent = likes + ' Me gusta';
-
-        // Almacenar la cantidad de "Me gusta" en el array
-        likesArray[index] = likes;
-
-        // Verificar la cantidad de "Me gusta" para la imagen actual
-        const totalLikes = likesArray[index];
-
-        if (totalLikes === 1) {
-            alert('¡Gracias por tu primer Me gusta!');
-        } else if (totalLikes === 3) {
-            alert('¡Muchas gracias! Ya me has dado 3 Me gusta');
-        } else {
-            alert('¡Gracias por tu Me gusta!');
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const nombre = prompt('Por favor, ingresa tu nombre:');
-    const saludoDiv = document.getElementById('saludo');
-
-    if (nombre) {
-        saludoDiv.textContent = '¡Hola ' + nombre + ', soy Rami!';
-    } else {
-        saludoDiv.textContent = 'Hola, soy Rami.';
+    // Función para guardar los "Me gusta" en localStorage
+    function guardarLikes() {
+        localStorage.setItem('likes', JSON.stringify(likesArray));
     }
+
+    // Mostrar los "Me gusta" almacenados en localStorage en los contadores
+    likeCounts.forEach((count, index) => {
+        count.textContent = likesArray[index] + ' Me gusta';
+    });
+
+    // Agregar eventos de clic a los botones "Me gusta"
+    likeBtns.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            likesArray[index]++;
+            likeCounts[index].textContent = likesArray[index] + ' Me gusta';
+            guardarLikes();
+        });
+    });
+
 });
-
-
 
