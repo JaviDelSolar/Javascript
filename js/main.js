e041bba15eed7c0767083d45182ddfc6e4b1fc36
Javiera Del Solar <javii.dsr@gmail.com>
2024-03-18 23:53:09 -0300

    Actualizacion codigo

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Acá obtengo los botones "Me gusta" y los contadores
     const likeBtns = document.querySelectorAll('.like-btn');
     const likeCounts = document.querySelectorAll('.like-count');
 
-    // Acá obtengo los "Me gusta" almacenados en localStorage o inicializarlos a cero
-    let likesArray = JSON.parse(localStorage.getItem('likes')) || Array.from({ length: likeBtns.length }, () => 0);
+    let likesArray = obtenerLikes() || Array.from({ length: likeBtns.length }, () => 0);
 
-    // Función para guardar los "Me gusta" en localStorage
-    function guardarLikes() {
-        localStorage.setItem('likes', JSON.stringify(likesArray));
+    function obtenerLikes() {
+        return localStorage.getItem('likes') ? localStorage.getItem('likes').split(',') : null;
     }
 
-    // Mostrar los "Me gusta" almacenados en localStorage en los contadores
+    function guardarLikes() {
+        localStorage.setItem('likes', likesArray.join(','));
+    }
+
     likeCounts.forEach((count, index) => {
         count.innerText = likesArray[index] + ' Me gusta';
     });
 
-    // Agregar eventos de clic a los botones "Me gusta"
     likeBtns.forEach((btn, index) => {
         btn.addEventListener('click', () => {
             likesArray[index]++;
@@ -25,4 +24,5 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
 
